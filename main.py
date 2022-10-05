0c6fd988f3e1c0e1286d64885da777e5b1a358ec
statik2002 <statik2002@mail.ru>
2022-10-05 18:07:27 +0800

    Шаг 12 Все разбил на отдельные функции

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from dotenv import load_dotenv
 import requests
 
 
-def get_comics_url(url):
+def get_comics_url_and_title(url):
 
     response = requests.get(url)
     response.raise_for_status()
 
-    return response.json()['img']
+    comics_info = response.json()
+
+    return comics_info['img'], comics_info['alt']
 
 
 def load_comics_image(url, folder):
@@ -36,7 +38,7 @@ def get_wall_upload_server(url, group_id, vk_token, v=5.131):
     }
     response = requests.get(f'{url}{method_photos_get_wall_upload_server}', params=params)
     response.raise_for_status()
-    return response.json()
+    return response.json()['response']['upload_url']
 
 
 def get_groups(url, vk_token, v=5.131, extended=1):
@@ -53,7 +55,22 @@ def get_groups(url, vk_token, v=5.131, extended=1):
     return response.json()
 
 
-def save_wall_photo(path, server, vk_token, group_id, v=5.131):
+def get_wall(vk_token, owner_id, v=5.131):
+
+    params = {
+        'access_token': vk_token,
+        'v': v,
+        'owner_id': -owner_id,
+        'filter': 'owner'
+    }
+
+    response = requests.get('https://api.vk.com/method/wall.get', params=params)
+    response.raise_for_status()
+
+    return response.json()
+
+
+def upload_photo_on_wall(path, server_url):
 
     with open(path, 'rb') as photo:
         files = {
@@ -61,16 +78,20 @@ def save_wall_photo(path, server, vk_token, group_id, v=5.131):
             'photo': photo
         }
 
-        response = requests.post(server, files=files)
+        response = requests.post(server_url, files=files)
         response.raise_for_status()
 
-        res2=response.json()
-        pprint(res2)
+        upload_response = response.json()
+
+        return upload_response['server'], upload_response['photo'], upload_response['hash']
+
+
+def save_wall_photo(server_id, photo, photo_hash, vk_token, group_id, v=5.131):
 
         params = {
-            'server': res2['server'],
-            'photo': res2['photo'],
-            'hash': res2['hash'],
+            'server': server_id,
+            'photo': photo,
+            'hash': photo_hash,
             'access_token': vk_token,
             'group_id': group_id,
             'v': v,
@@ -79,7 +100,26 @@ def save_wall_photo(path, server, vk_token, group_id, v=5.131):
         response = requests.post('https://api.vk.com/method/photos.saveWallPhoto', params=params)
         response.raise_for_status()
 
-        pprint(response.json())
+        save_photo_response = response.json()
+
+        return save_photo_response['response'][0]['owner_id'], save_photo_response['response'][0]['id']
+
+
+def post_photo_on_wall(vk_group_id, title, photo_owner_id, photo_id, vk_token, v=5.131):
+
+    params = {
+        'v': v,
+        'access_token': vk_token,
+        'owner_id': f'-{vk_group_id}',
+        'from_group': -1,
+        'message': title,
+        'attachments': {
+            f'photo{photo_owner_id}_{photo_id}'
+        }
+    }
+
+    response = requests.post('https://api.vk.com/method/wall.post', params=params)
+    response.raise_for_status()
 
     return response.json()
 
@@ -94,31 +134,25 @@ def main():
 
     url = 'https://xkcd.com/info.0.json'
 
-    comics_url = get_comics_url('https://xkcd.com/info.0.json')
+    comics_url, comics_title = get_comics_url_and_title('https://xkcd.com/info.0.json')
     destination_folder = 'Comics'
     Path(destination_folder).mkdir(exist_ok=True)
-
     comics_image_path = load_comics_image(comics_url, destination_folder)
-    #print(comics_image_path)
 
     url_vk = 'https://api.vk.com/method/'
 
-    #get_all_groups = get_groups(url_vk, vk_token)
-    #pprint(get_all_groups)
+    # Получили ссылку для загрузки на сервер
+    upload_server_url = get_wall_upload_server(url_vk, vk_group_id, vk_token)
 
-    upload_server = get_wall_upload_server(url_vk, vk_group_id, vk_token)
-    #pprint(upload_server)
+    # Загрузили фото на стену
+    server_id, photo, photo_hash = upload_photo_on_wall(os.path.abspath(comics_image_path), upload_server_url)
 
+    # Сохранили фото на стене
+    saved_owner_id, saved_photo_id = save_wall_photo(server_id, photo, photo_hash, vk_token, vk_group_id)
 
-    #print(os.path.abspath(comics_image_path))
-
-    upload_photo_response = save_wall_photo(
-        os.path.abspath(comics_image_path),
-        upload_server['response']['upload_url'],
-        vk_token,
-        vk_group_id
-    )
-    #print(upload_photo_response)
+    # Опубликовали фото
+    res2 = post_photo_on_wall(vk_group_id, comics_title, saved_owner_id, saved_photo_id, vk_token)
+    #pprint(res2)
 
 
 if __name__ == '__main__':
